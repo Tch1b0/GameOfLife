f5293ed6f4cab2b83d8dfebb0bade0bbedd31070
Tch1b0 <Jpour3006@gmail.com>
2021-06-03 16:00:51 +0200

    initial commit

--- site/script.js
+++ site/script.js
@@ -2,22 +2,25 @@ var height = 800;
 var width = 1000;
 var density = 25;
 var c = document.getElementById("field");
+var ctx = c.getContext("2d");
+var changeDensity = document.getElementById("changeSize")
+var grid = []
 
 function drawField(ctx, width, height, density) {
-    let grid = []
+    let locGrid = [[]]
 
     for (let i=1; i < width; i++) {
         if(i % density == 0){
             ctx.moveTo(i, 0);
             ctx.lineTo(i, height);
             ctx.stroke();
-            grid.push([]);
+            locGrid.push([]);
         }
     }
 
     // The first column has to be added manually
     for (let n=0; n < width/density; n++) {
-        grid[0].push(false);
+        locGrid[0].push(false);
     }
 
     for (let i=1; i < height; i++) {
@@ -30,11 +33,11 @@ function drawField(ctx, width, height, density) {
 
             // Append every field to the array
             for (let n=0; n < width/density; n++) {
-                grid[i / density].push(false);
+                locGrid[i / density].push(false);
             }
         }
     }
-    return grid
+    return locGrid
 }
 
 function getMousePos(canvas, evt) {
@@ -47,14 +50,17 @@ function getMousePos(canvas, evt) {
 
 function mouseMove(event) {
     let mp = getMousePos(c, event);
-    
 }
 
 function main() {
-    let ctx = c.getContext("2d");
-    let grid = drawField(ctx, width, height, density);
+    grid = drawField(ctx, width, height, density);
 
-    c.addEventListener("mousemove", mouseMove, false);
+    c.addEventListener("mousedown", mouseMove, false);
 }
 
 main()
+
+changeDensity.addEventListener("change", (evt) => {
+    density = changeDensity.value
+    drawField(ctx, width, height, density)
+}, false);
